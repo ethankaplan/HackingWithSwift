09f5264e2ba728ac9c1fb8a9b04ed0d55a0fa301
Ethan Kaplan <ethankaplan@Mac.attlocal.net>
2024-12-14 12:13:25 -0800

    Initializing git; project 1, 2 finished

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ethan Kaplan on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
